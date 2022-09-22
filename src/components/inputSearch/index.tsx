e2479f653a49cde3ffcc9a8b3d4829a4018877b2
Kiên <86771355+toilakien@users.noreply.github.com>
2022-09-22 13:26:56 +0700

    update input

--- src/components/inputSearch/index.tsx
+++ src/components/inputSearch/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ips.css';
 import img from '../../resource/image/1024px-Search_Icon.svg.png'
 const InputSearch = () => {
+  const [open, setOpen] = useState<any>("");
+  const handleFocus = () => {
+    setOpen("boder")
+  }
+  const fcn = () => {
+    setOpen("")
+  }
   return (
-    <div className='input-search'>
+    <div className={`input-search ${open}`}>
       <img src={img} alt="" />
-      <input placeholder='Tìm kiếm theo mã khách hàng ,tên ,SĐT khách hàng' />
+      <input onFocus={handleFocus} onBlur={fcn} placeholder='Tìm kiếm theo mã khách hàng ,tên ,SĐT khách hàng' />
     </div>
   )
 }
