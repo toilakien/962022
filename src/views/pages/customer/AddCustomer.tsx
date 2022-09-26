c0416ca779df61ddb37e30046a5ac93a5e93801a
Kiên <86771355+toilakien@users.noreply.github.com>
2022-09-26 13:28:25 +0700

    fix layout

--- src/views/pages/customer/AddCustomer.tsx
+++ src/views/pages/customer/AddCustomer.tsx
@@ -1,12 +1,15 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material'
-
+import { Autocomplete, Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, TextField } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close';
 const AddCustomer = ({ open, setOpen }: { open: boolean, setOpen: any }) => {
     console.log(open);
     const handleClose = () => {
         setOpen(!open)
     }
     return (
-        <Dialog open={open} onClose={handleClose}>
+        <Dialog maxWidth="lg" open={open} onClose={handleClose} sx={{ p: 5 }}>
+            <IconButton onClick={handleClose} sx={{ position: "absolute", top: 5, left: "88%", color: "#111", width: "5px", height: "5px", p: 2, border: "1px solid #111" }}>
+                <CloseIcon />
+            </IconButton>
             <DialogTitle>
                 ADD CUSTOMER
             </DialogTitle>
@@ -18,12 +21,13 @@ const AddCustomer = ({ open, setOpen }: { open: boolean, setOpen: any }) => {
                     fullWidth
                     margin='normal'
                 />
-                <TextField
-                    id="status"
-                    label="Status"
-                    variant='standard'
-                    fullWidth
-                    margin='normal'
+                <Autocomplete
+                    disablePortal
+                    id="combo-box-demo"
+                    options={["True", "Flase"]}
+                    sx={{ width: 300 }}
+                    renderInput={(params: any) => <TextField {...params} variant="standard" label="Status" fullWidth margin='normal' />}
+
                 />
                 <TextField
                     id="name"
@@ -34,7 +38,7 @@ const AddCustomer = ({ open, setOpen }: { open: boolean, setOpen: any }) => {
                 />
             </DialogContent>
             <DialogActions>
-                <Button variant='contained' fullWidth>Save</Button>
+                <Button variant='contained' fullWidth sx={{ backgroundColor: "#111", "&:hover": { backgroundColor: "#222" } }} >Save</Button>
             </DialogActions>
         </Dialog>
     )
